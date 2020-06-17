489183213491059bef5d5822f41ab814dcb35850
Tristan Guest <tristanguest@gmail.com>
2020-06-17 11:21:44 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,16 +4,8 @@ var playlist = {
   'Phil Ochs': "Here's to the State of Mississippi"
 }
 
-// var playlist = {
-//   'Mylo': "Otto's Journey"
-//   }
-  
 console.log(playlist)  
 
-
-// function updatePlaylist(playlist, artist, song) {
-//   playlist = Object.assign({}, playlist, { [artist]: song } )
-// }
 
 function updatePlaylist(playlist, artist, song) {
   playlist[artist] = song
@@ -25,7 +17,6 @@ function removeFromPlaylist(playlist, artist) {
   return playlist
 }
 
-// console.log(delete playlist['Mylo'])
 console.log(removeFromPlaylist(playlist, 'Mylo'))
 
 
