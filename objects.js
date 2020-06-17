a54cc53cfe80978d4a99b22a8f5525c4326c653d
Tristan Guest <tristanguest@gmail.com>
2020-06-17 11:11:44 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,30 @@
+// var playlist = {
+//   'Slowdive': 'Alison', 
+//   'My Bloody Valentine': 'Sometimes', 
+//   'Phil Ochs': "Here's to the State of Mississippi"
+// }
+
+var playlist = {
+  'Mylo': "Otto's Journey"
+  }
+
+
+  
+console.log(playlist)  
+
+
+// function updatePlaylist(playlist, artist, song) {
+//   playlist = Object.assign({}, playlist, { [artist]: song } )
+// }
+
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song
+}
+
+console.log(playlist, 'Beck', 'Paper Tiger')
+
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist.artist;
+}
+
